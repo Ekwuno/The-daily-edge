a1f653c88c9df2624bd5923f9b4661224880940a
Ekwuno <obinnacodes@gmail.com>
2022-07-12 03:54:06 +0100

    Add form hadnler logic

--- functions/api/form.js
+++ functions/api/form.js
@@ -1,13 +1,21 @@
-export async function onRequestPost ({request}){
-    try {
-        const response = request.json()
-        console.log(response)
-        
-    } catch (err) {
-        return new Response("This does not work Obinna",{
-            status:400,
-            error:err
-        })
-    }
-    return new Response("new yello")
+export async function onRequestPost({ request, env }) {
+	try {
+		const response = await request.json();
+		const uuid = crypto.randomUUID();
+		const form = JSON.stringify({ uuid, ...response }, null, 2);
+
+		await env.comment_store.put(uuid, form, {
+			metadata: { createdAt: new Date() },
+		});
+		return new Response(JSON.stringify(response), {
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8',
+			},
+		});
+	} catch (err) {
+		return new Response('This does not work Obinna', {
+			status: 400,
+			error: err,
+		});
+	}
 }
