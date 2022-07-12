e736d6764768db0fe8b1f92fc80859d9e24f0b16
Ekwuno <obinnacodes@gmail.com>
2022-07-12 03:32:27 +0100

    setup repo and add chakra

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, Container, Heading } from "@chakra-ui/react";
+import { AddComment } from "./components/AddComment";
+import { theme } from "./theme";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+    <Container maxW="container.lg" py={16} px={8} bg="white" minH="100vh">
+      <Heading as="h2" size="2xl">
+       The Daily Edge
+      </Heading>
+<AddComment/>
+    </Container>
+  </ChakraProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      "html, body": {
+        lineHeight: "tall",
+        backgroundColor: "gray.50",
+        height: "100%",
+      },
+    },
+  },
+});
