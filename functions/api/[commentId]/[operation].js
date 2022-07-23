0b7b327ff30e1f62a4296d305f614ea4c62faa22
Obinna Ekwuno <obinna@cloudflare.com>
2022-07-23 01:47:11 +0100

    calm

--- functions/api/[commentId]/[operation].js
+++ functions/api/[commentId]/[operation].js
@@ -1,6 +1,6 @@
 export async function onRequest({ request, params, env }) {
 	try {
-       let commentID = params.commentID;
+        const { commentID, operation } = params;
 				let id = env.Learning_DurableObjects.idFromName(commentID); // Create a new unique ID
 				let stub = env.Learning_DurableObjects.get(id);
                 console.log(typeof commentID); 
