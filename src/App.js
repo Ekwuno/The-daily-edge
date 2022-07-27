2a4dc460e4ca8165576b32ce578a77c8ea5d2b7d
Obinna Ekwuno <obinna@cloudflare.com>
2022-07-27 21:31:30 +0100

    pushing creds

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { ChakraProvider, Container, Heading } from '@chakra-ui/react';
 import CommentSection from './components/CommentSection';
+import { AuthProvider } from "react-auth-kit";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { theme } from './theme';
 
 function App() {
 	return (
-		<ChakraProvider theme={theme}>
-			<Container maxW="container.lg" py={16} px={8} bg="white" minH="100vh">
-				<Heading as="h2" size="2xl">
-					The Daily Edge
-				</Heading>
-				<CommentSection />
-			</Container>
-		</ChakraProvider>
+		<GoogleOAuthProvider clientId="667479857185-5e3gtvc9nd9br5htl9qhsf0ub0nape6t.apps.googleusercontent.com">
+			<ChakraProvider theme={theme}>
+				<Container maxW="container.lg" py={16} px={8} bg="white" minH="100vh">
+					<Heading as="h2" size="2xl">
+						The Daily Edge
+					</Heading>
+					<CommentSection />
+				</Container>
+			</ChakraProvider>
+		</GoogleOAuthProvider>
 	);
 }
 
