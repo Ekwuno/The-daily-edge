e0c801e6073509697edf14f5b2aec962d501f463
Obinna Ekwuno <obinna@cloudflare.com>
2022-08-01 09:10:16 +0100

    remove header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading,Icon } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import CommentSection from './components/CommentSection';
 import Navbar from './components/Navbar';
 import qs from 'query-string';
 import { useAuth } from './context/AuthContext';
+
 
 function App() {
 	const { login } = useAuth();
@@ -24,9 +25,6 @@ function App() {
 		<>
 			<Navbar />
 			<Box px={8} py={16}>
-				<Heading as="h2" size="2xl">
-					The Daily Edge
-				</Heading>
 				<CommentSection />
 			</Box>
 		</>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Flex, Icon, Image, Link } from '@chakra-ui/react';
-import { AiFillGithub } from 'react-icons/ai';
+import { AiFillGithub,AiFillBook } from 'react-icons/ai';
 import { MdLogout } from 'react-icons/md';
 import { useAuth } from '../context/AuthContext';
 import { getFormattedGithubUrl } from '../utils/urlFormatter';
@@ -12,14 +12,15 @@ const Navbar: React.FC = () => {
 	const { user, logout } = useAuth();
 	return (
 		<Flex align="center" justify="space-between" px={4} py={2} boxShadow="sm">
-			<Image
+			{/* <Image
 				src="https://b0.thejournal.ie/desktop/i/thedailyedge/landscape.png"
 				width="50px"
 				height="50px"
 				alt="The Daily Edge"
 				objectFit="cover"
 				rounded="full"
-			/>
+			/> */}
+			<Icon as={AiFillBook} size="2xl" w={8} h={8} />
 			{user ? (
 				<Flex gap={4}>
 					<Image
